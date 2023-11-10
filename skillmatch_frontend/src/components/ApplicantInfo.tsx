f23903000d6deb8a575cb3b1a95bfa54f7fc37b8
Alan Park <28246519+alansdpark@users.noreply.github.com>
2023-11-10 04:41:45 -0800

    Added Dashboard and Basic Applicant skeletons

---
+++ skillmatch_frontend/src/components/ApplicantInfo.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { Avatar, Grid, Container } from "@mui/material";
+import useApplicationsByJob from "../hooks/useApplicationsByJob";
+
+const ApplicantInfo = () => {
+    const { applications, setApplications, error } =
+        useApplicationsByJob("234253");
+    console.log(applications);
+
+    return (
+        <Container maxWidth="sm">
+            <Grid container spacing={2}>
+                <Grid item xs={1}>
+                    {/* Add from applicant data */}
+                    <Avatar style={{ marginTop: "1em" }} />
+                </Grid>
+                <Grid item xs={10}>
+                    <h1 style={{ marginLeft: "0.25em" }}>First Last</h1>
+                </Grid>
+            </Grid>
+            <Grid container spacing={1}>
+                <Grid item xs={12}>
+                    <h3>Experience</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        A magnam dolor, eveniet adipisci voluptatum veniam ipsam
+                        aspernatur necessitatibus optio impedit enim, voluptates
+                        velit nobis saepe. Minus numquam recusandae voluptates
+                        commodi.
+                    </p>
+                </Grid>
+                <Grid item xs={12}>
+                    <h3>Education</h3>
+                </Grid>
+                <Grid item xs={12}>
+                    <h3>Skills</h3>
+                </Grid>
+            </Grid>
+        </Container>
+    );
+};
+export default ApplicantInfo;
