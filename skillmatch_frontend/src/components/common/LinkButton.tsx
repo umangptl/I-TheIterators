8da98380c49bd04e6e8c8b17e1a0cbcb8f2e1dad
gemj98 <74983768+gemj98@users.noreply.github.com>
2023-11-26 23:27:05 -0800

    Delete test/extra files, update application properties in backend (#31)

--- skillmatch_frontend/src/components/common/LinkButton.tsx
+++ skillmatch_frontend/src/components/common/LinkButton.tsx
@@ -2,7 +2,7 @@ import { Button, ButtonProps } from "@mui/material";
 import { styled } from "@mui/system";
 import { Link, LinkProps } from "react-router-dom";
 
-const LinkButton = styled(Button)<ButtonProps & LinkProps>();
+const LinkButton = styled(Button)<ButtonProps & LinkProps>(({ theme }) => ({}));
 LinkButton.defaultProps = {
   variant: "contained",
   color: "success",
