dc581dc236015e71d7239e65a7e6fdda4ff07e08
Abhilash <tayadeabhilash@gmail.com>
2023-12-03 13:28:32 -0800

    Fix other endpoints

--- SkillMatch/src/main/java/com/iterators/skillmatch/repository/ApplicationRepository.java
+++ SkillMatch/src/main/java/com/iterators/skillmatch/repository/ApplicationRepository.java
@@ -8,9 +8,11 @@ import java.util.List;
 
 public interface ApplicationRepository extends MongoRepository<Application, String> {
 
+    @Query(value = "{}", fields = "{ 'applicant.resume' : 0 }")
     List<Application> findByApplicant_Email(String emailId);
     @Query(value = "{}", fields = "{ 'applicant.resume' : 0 }")
     List<Application> findAllWithoutResume();
+    @Query(value = "{}", fields = "{ 'applicant.resume' : 0 }")
     List<Application> findByJobId(String jobId);
     Application findByApplicant_EmailAndJobId(String emailId, String jobId);
     Integer countApplicationsByApplicant_EmailAndJobId(String emailId, String jobId);
